76d68d56c9ba92acdb4836e509e8ca44dd02449c
lisa.cresti@stud.unifi.it <lisa.cresti@stud.unifi.it>
2018-03-22 15:29:10 +0100

    Main

--- main.cpp
+++ main.cpp
@@ -5,17 +5,13 @@ int main() {
     try {
         MatrixTemplate<float> a(3, 4);
         MatrixTemplate<float> b(4, 2);
-        SquareMatrixTemplate<float> c(2);
         a.setValue(2, 2, 4.5);
         a.setValue(3, 1, 6.7);
         b.setValue(2, 2, 2);
         b.setValue(3, 1, 2);
-        c.setValue(2, 2, 1);
-        c.setValue(1,1,1);
         MatrixTemplate<float> d(a.getRows(), b.getColumns());
         d = a*b;
         d.print();
-        c.print();
     }
     catch(std::out_of_range &e){
         std::cerr << e.what() << std::endl;
